d96ba8d48bfc46ea064eea66db54cb35c0be6161
Cierra <134563934+CMoret@users.noreply.github.com>
2023-09-03 02:43:54 -0500

    Update PasswordHash.py

--- PasswordHash.py
+++ PasswordHash.py
@@ -4,11 +4,12 @@ import bcrypt
 #password used
 password = b'Hello World'
 
-#salt generator
+#salts the password
 salt = bcrypt.gensalt()
 
-#hashes password
+#hashes the password
 hasher = bcrypt.hashpw(password,salt)
 
+#prints out hash
 print('Your password hashed is: ')
 print(hasher)
